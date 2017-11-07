33171cf912666ec553c472bd7884bd813b37c770
Erin Blauw <erin@blue-arachnid.com>
2017-11-07 19:49:48 +0100

    setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27018/Boodschapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27018/Boodschapp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {User} = require('./models/user');
 const {List} = require('./models/list');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -51,8 +52,8 @@ app.get('/lists/:id', (request, response)=> {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Started on port 3000");
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
